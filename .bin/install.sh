8c9d482c283be538ed8471f0d67102c28496c917
pp253 <pp.pp253@gmail.com>
2018-09-26 18:02:24 +0800

    update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,36 +1,46 @@
-sudo apt-get update
+echo "Remember to run the script with privileges!"
 
-sudo apt-get install -y nmap
-sudo apt-get install -y dnsutils
-sudo apt-get install -y git
-sudo apt-get install -y ntp
-echo "Remember to sudo vim /etc/ntp.conf"
+userhome=/home/linaro
 
-sudo apt-get install -y dirmngr --install-recommends
-curl -sL https://deb.nodesource.com/setup | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-sudo npm i pm2 webpack nodemon babel-node babel-env -g
+echo "Update the packages..."
+apt-get update
 
+echo "Install the system tools..."
+apt-get install -y nmap
+apt-get install -y dnsutils
+apt-get install -y dirmngr --install-recommends
 
-cd ~/Downloads
-wget http://dlcdnet.asus.com/pub/ASUS/mb/Linux/Tinker_Board_2GB/GPIO_API_for_Python.zip
-unzip GPIO_API_for_Python.zip
-sudo python setup.py install
-sudo python3 setup.py install
-
-sudo apt-get install -y pip
-sudo apt-get install -y python3-pip
-sudo apt-get install -y ipython
-sudo apt-get install -y python-numpy python-scipy python-matplotlib python-sympy python-nose
-sudo apt-get install -y python3-setuptools
-
-sudo apt-get autoremove
-
+echo "Install and configurate the git..."
+apt-get install -y git
 git config --global user.name "pp253"
 git config --global user.email "pp.pp253@gmail.com"
 
-cd ~/Documents
+echo "Install and configurate the ntp..."
+apt-get install -y ntp
+mv /etc/ntp.conf /etc/ntp.conf.bak
+sed '15iline server time.stdtime.gov.tw' /etc/ntp.conf.bak > /etc/ntp.conf
+
+echo "Install the NodeJS and npm, and the recommended modules..."
+curl -sL https://deb.nodesource.com/setup | -E bash -
+apt-get install -y nodejs
+apt-get install -y npm
+npm i pm2 webpack nodemon babel-node babel-env -g
+
+cd $userhome/Downloads
+wget http://dlcdnet.asus.com/pub/ASUS/mb/Linux/Tinker_Board_2GB/GPIO_API_for_Python.zip
+unzip GPIO_API_for_Python.zip
+python setup.py install
+python3 setup.py install
+
+apt-get install -y pip
+apt-get install -y python3-pip
+apt-get install -y ipython
+apt-get install -y python-numpy python-scipy python-matplotlib python-sympy python-nose
+apt-get install -y python3-setuptools
+
+apt-get autoremove
+
+cd $userhome/Documents
 mkdir ./Coding
 git clone https://github.com/pp253/wettycat.git
 git clone https://github.com/pp253/wettycat-dht11.git
@@ -42,7 +52,9 @@ npm run move
 cp ./.bin/ecosystem.config.json ../ecosystem.config.json
 
 cd ../wettycat-dht11
-sudo pip install requests
+pip install requests
 
 cd ..
-sudo pm2 start ecosystem.config.json
+pm2 startup
+pm2 start ./ecosystem.config.json
+pm2 save
